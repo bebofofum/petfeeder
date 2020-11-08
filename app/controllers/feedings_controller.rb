9ecf317bf4c3e1d72d501326889a02ed7acc104b
bebofofum <bri.boehm@gmail.com>
2020-11-07 21:09:56 -0500

    added actions to controller

--- app/controllers/feedings_controller.rb
+++ app/controllers/feedings_controller.rb
@@ -1,2 +1,29 @@
 class FeedingsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def feeding_params
+    params.require(:feeding).permit(:user_id, :pet_id, :completed, :description, :category)
+  end
 end
