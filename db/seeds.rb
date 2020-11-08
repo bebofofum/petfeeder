fcf955701a23ee2951a0ef02c61e99c7f3b85541
bebofofum <bri.boehm@gmail.com>
2020-11-07 21:13:49 -0500

    added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user2 = User.find_or_create_by(name: "Kelp", email: "this@that.com", password: "password")
+
+pet1 = Pet.find_or_create_by(name: "Fin", species: "Bearded Dragon")
+pet2 = Pet.find_or_create_by(name: "Zuhl", species: "Bearded Dragon")
+pet3 = Pet.find_or_create_by(name: "Zilla", species: "Axolotl")
+pet4 = Pet.find_or_create_by(name: "Monsta", species: "Gecko")
+
+feed1 = pet1.feedings.find_or_create_by(
+  user: user1,
+  category: "Evening",
+  completed: "false"
+)
+
+feed2 = pet3.feedings.find_or_create_by(
+  user: user1,
+  category: "Morning",
+  description: "Gave it 3 worms, cut in half",
+  completed: "true"
+)
+
+
+
