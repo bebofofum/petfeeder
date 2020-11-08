5132bd42c33a49ac37666e995fa136e22de5d7ce
bebofofum <bri.boehm@gmail.com>
2020-11-08 15:10:14 -0500

    Adding crud restful routes

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,15 +1,29 @@
 class PetsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
+
 
   def index
+       @pets = Pet.all
   end
 
   def show
+  
   end
 
   def new
+    @pet = Pet.new
   end
 
   def create
+    @pet = Pet.new(pet_params)
+
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new
+    end
+
   end
 
   def edit
@@ -20,11 +34,18 @@ class PetsController < ApplicationController
 
   def destroy
   end
+  
 
   private
 
   def pet_params
     params.require(:pet).permit(:name)
   end
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
+
 
 end
