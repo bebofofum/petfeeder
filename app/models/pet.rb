cdf4eb303dad4578f38df2dd648fb14be523ffb7
bebofofum <bri.boehm@gmail.com>
2020-11-07 21:12:33 -0500

    Moved erroneously placed actions and added scope method

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,36 +3,15 @@ class Pet < ApplicationRecord
   has_many :users, through: :feedings
 
   #validations
-  validates :name, :type, :feeding_details, presence: true
+  validates :name, :species, :diet_requirements, presence: true
   validates :name, length: { minimum: 2 }
   validates :name, uniqueness: true
 
-
-  def index
+  #scope method
+  def special_diet?
+    where(special_needs: true)
   end
 
-  def show
-  end
 
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-  private
-
-  def pet_params
-    params.require(:artist).permit(:name)
-  end
 
 end
