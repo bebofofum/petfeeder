3b63081050cd94aa7995653fd208c44a4f24fccc
bebofofum <bri.boehm@gmail.com>
2020-11-08 15:05:14 -0500

    Added rescue method to display error if record isn't found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+
+  private
+  
+  def record_not_found
+    flash[:error] = "Not Found"
+    redirect_to pets_path
+  end
+
+
 
   protected
 
