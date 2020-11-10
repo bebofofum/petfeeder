58adce82bc60a66a6121dfa6983161536ecde90c
bebofofum <bri.boehm@gmail.com>
2020-11-09 19:17:45 -0500

    created some helper methods

--- app/models/feeding.rb
+++ app/models/feeding.rb
@@ -4,5 +4,16 @@ class Feeding < ApplicationRecord
 
   validates :category, inclusion: { in: %w(Morning Evening),
   message: "Must be either Morning or Evening" }
+  validates :description, presence: true
+  
+
+  def user_name_normalize
+    user.name.capitalize
+  end
+
+  def pet_name
+    pet.name
+  end
+
 
 end
