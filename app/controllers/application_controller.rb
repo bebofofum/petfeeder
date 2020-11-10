bef37e91e60c314f81c72600892530e03546e344
bebofofum <bri.boehm@gmail.com>
2020-11-09 19:14:48 -0500

    created helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,21 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  helper_method :current_pet
+
+
+
 
   private
   
   def record_not_found
     flash[:error] = "Not Found"
     redirect_to pets_path
+  end
+
+  def current_pet
+    # @pet ||= Pet.find(params[:id])
+    Pet.find(params[:id])
   end
 
 
