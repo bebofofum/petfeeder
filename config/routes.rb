f42e0696bf9bec4649c668e76fa5a6dbde16d250
bebofofum <bri.boehm@gmail.com>
2020-11-12 13:55:52 -0500

    Added omniauth callback for google

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :feedings, only: [:new, :index, :show]
   end 
   
-  devise_for :users 
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } 
 
   root 'pets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
