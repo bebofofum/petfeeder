7c264ce2517d554755bc56c11dba1364410ee5ed
bebofofum <bri.boehm@gmail.com>
2020-11-12 13:52:45 -0500

    Added google oauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
+  def self.from_google(uid:, email:, full_name:, avatar_url:)
+   if user = User.find_by(email: email)
+      user.update(uid: uid, full_name: full_name, avatar_url: avatar_url) unless user.uid.present?
+      user
+    else
+      User.create(
+        email: email,
+        uid: uid,
+        full_name: full_name,
+        avatar_url: avatar_url,
+        password: SecureRandom.hex
+      )
+    end
+  end
   
-
-
 end
